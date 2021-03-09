384f98f138443c72a64c4f2dfdf7d392e8a10121
Fionn Bracken <fionnbracken@Fionns-MacBook-Air.local>
2021-03-09 22:37:35 +0000

    email html

--- chessemail.py
+++ chessemail.py
@@ -4,10 +4,15 @@ from datetime import timedelta, datetime
 from chessdotcom import get_player_stats, get_player_games_by_month
 import berserk
 import os
+import io
+import smtplib
+from email.message import EmailMessage
 
-# Retrieve envrionment variables of my lcihess API token and my username
+# Retrieve envrionment variables
 username = os.environ.get('chessname')
 apitoken = os.environ.get('lichess_api_token')
+emailaddress = os.environ.get('email')
+emailpassword = os.environ.get('emailpass')
 
 # Get datetime for lastweek
 lastweek = datetime.now()  - timedelta(days=7)
@@ -15,30 +20,30 @@ year, month = lastweek.year, lastweek.month
 
 # Get my chess.com personal stats
 stats = get_player_stats(username).json
-
+"""
 categories = ['chess_bullet', 'chess_blitz', 'chess_rapid', 'chess_daily']
 print('Chess.com Current Rankings')
 for category in categories:
     print(category[6:] + ": " + str(stats[category]['last']['rating']))
-"""    
+ 
 print('\nBest')
 for category in categories:
     print(category[6:] + ": " + str(stats[category]['best']['rating']))
-"""    
+  
 print('\nPuzzle Rush')
 print('Best: ' + str(stats['puzzle_rush']['best']['score']))
-
+"""
 # Set up lichess API session
 session = berserk.TokenSession(apitoken)
 client = berserk.Client(session=session)
-
+"""
 # Get my lichess personal stats
 profile = client.account.get()
 categories2 = ['bullet', 'blitz', 'rapid']
 print("\nLichess Current Rankings")
 for category in categories2:
     print(category + ": " + str(profile['perfs'][category]['rating']))
-
+"""
 # Get my chess.com games from the last week
 chesscom_games = get_player_games_by_month('flbrack', year, month).json['games']
 
@@ -92,7 +97,7 @@ total_games = len(lichess_games) + len(chesscom_games)
 total_wins = lichess_wins + chesscom_wins
 total_losses = lichess_losses + chesscom_losses
 total_draws = lichess_draws + chesscom_draws
-
+"""
 sizes = [total_wins, total_draws, total_losses]
 labels = ['wins', 'draws', 'losses']
 
@@ -102,7 +107,52 @@ for i in range(3):
 
 plt.pie(sizes, labels = labels, startangle = 270, autopct='%1.1f%%')
 plt.title(f"Total Games: {total_games}")
-plt.show()
+plt.savefig("Piechart.png")
+"""
+
+
+msg = EmailMessage()
+msg['Subject'] = 'Your Weekly Chess Update'
+msg['From'] = emailaddress
+msg['To'] = emailaddress
+
+msg.set_content('HTML is not working for your email client')
+
+msg.add_alternative(f"""\
+<!DOCTYPEhtml>
+<html>
+    <body> 
+        <h1>Your Chess Stats From Last Week</h1>
+        <p>
+        Total Games Played: {total_games}
+        <br>
+        Lichess Games Played: {len(lichess_games)}
+        <br>
+        Chess.com Games Played: {len(chesscom_games)}
+        <br>
+        <br>
+        Wins: {total_wins}
+        <br>
+        Losses: {total_losses}
+        <br>
+        Draws: {total_draws}
+        </p>
+    </body>
+</html>
+""", subtype='html')
+
+
+with smtplib.SMTP('smtp.office365.com', 587) as smtp:
+    try:
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.ehlo()
+
+        smtp.login(emailaddress, emailpassword)
+        smtp.send_message(msg)
+    except:
+        print("Error: Failed to send email.")
+
 
 
 
